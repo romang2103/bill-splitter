b335c498838e0a98b37c6b8a74faaab33347794f
Roman Gurung <romang2103@gmail.com>
2024-04-10 05:14:17 +0100

    Refactoring JSON response

--- backend/test.py
+++ backend/test.py
@@ -2,20 +2,23 @@
 from google.api_core.client_options import ClientOptions
 from google.cloud import documentai
 import os
+import re
 
 
 def main():
     PROJECT_ID = "evensteven"
     LOCATION = "eu"  # Format is 'us' or 'eu'
-    PROCESSOR_ID = "3ea9f8f4c9efbd50"  # Create processor in Cloud Console
+    PROCESSOR_ID = "9f4ba5473e27e98f"  # Create processor in Cloud Console
 
-    # credential_path = "C:\\Users\\Roman\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
-    credential_path = "C:\\Users\\roman\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
+    credential_path = "C:\\Users\\Roman\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
+    # credential_path = "C:\\Users\\roman\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credential_path
 
     # The local file in your current working directory
+    FILE_PATH = "E:\\GitHub\\bill-splitter\\assets\\bill2.jpg"
+
     # FILE_PATH = "E:\\GitHub\\bill-splitter\\assets\\bill2.jpg"
-    FILE_PATH = "C:\\Users\\roman\\Documents\\GitHub\\bill-splitter\\assets\\bill2.jpg"
+    # FILE_PATH = "C:\\Users\\roman\\Documents\\GitHub\\bill-splitter\\assets\\bill2.jpg"
 
     # Refer to https://cloud.google.com/document-ai/docs/file-types
     # for supported file types
@@ -50,17 +53,53 @@ def main():
     print("Document processing complete.")
 
     # Write form fields to a separate file
-    with open("form_fields.txt", "w") as output_file:
-        for page_num, page in enumerate(document_object.pages):
-            output_file.write(f"Page {page_num + 1} Form Fields:\n")
-            for form_field_num, form_field in enumerate(page.form_fields):
-                output_file.write(
-                    f"Form Field {form_field_num + 1}: {form_field.field_value.text_anchor.content}\n"
-                )
-                output_file.write("\n")
+    # with open("form_fields.txt", "w") as output_file:
+    #     for page_num, page in enumerate(document_object.pages):
+    #         # output_file.write(f"Page {page_num + 1} Form Fields:\n")
+    #         for form_field_num, form_field in enumerate(page.form_fields):
+    #             output_file.write(f"Form Field {form_field_num + 1}\n")
+    #             output_file.write(f"Name: {form_field.field_name.text_anchor.content}")
+    #             output_file.write(
+    #                 f"Value: {form_field.field_value.text_anchor.content}\n"
+    #             )
+    #             output_file.write("\n")
 
-                # Test pages.tables -- check for field_values
+    entities = [
+        {
+            "Type": entity.type_,
+            "Properties": [
+                {
+                    "Type": prop.type_,
+                    "Content": prop.text_anchor.content,  # Adjust as necessary to match your actual data structure
+                }
+                for prop in entity.properties
+            ],
+        }
+        for entity in document_object.entities
+        if entity.type_ == "line_item"
+    ]
+    total_amount = 0
+    total_tax_amount = 0
+    
+    for entity in document_object.entities:
+        if entity.type_ == "total_amount":
+            total_amount = entity.mention_text
+        elif entity.type_ == "total_tax_amount":
+            total_tax_amount = entity.mention_text
 
+    # with open("processed_response_tesco.txt", "w") as output_file:
+    #     for form_field_num, form_field in enumerate(form_fields):
+    #         output_file.write(f"Form Field {form_field_num + 1}\n")
+    #         output_file.write(f"Name: {form_field['Name']}\n")
+    #         output_file.write(f"Value: {form_field['Value']}\n\n")
+
+    with open("response_invoice.txt", "w") as output_file:
+        for entity_num, entity in enumerate(entities):
+            # output_file.write(f"Entity {entity_num + 1}\n")
+            # output_file.write(f"Type: {entity["Type"]}\n")
+            output_file.write(f"Properties: {entity["Properties"]}\n\n")
+        output_file.write(f"Tax: {total_tax_amount}\n")
+        output_file.write(f"Total: {total_amount}")
 
 if __name__ == "__main__":
     main()
